11bbba276a5b3df97d55e2eba1d7a45b7025bc48
guibaumer <117869143+guibaumer@users.noreply.github.com>
2023-01-31 21:09:16 -0300

    otgw finais

--- OTGW/index.js
+++ OTGW/index.js
@@ -1,3 +1,7 @@
+const span2 = document.querySelector('.span-hidden-menu');
+let largura = window.innerWidth;
+const menu2 = document.querySelector('.hidden-menu');
+
 const span = document.querySelector('.material-symbols-outlined');
 const body = document.querySelector('body');
 const menu = document.querySelector('#invisible');
@@ -5,11 +9,16 @@ const xis = document.querySelector('.fecha-menu')
 
 span.addEventListener('click', function() {
     mostraMenu();
-})
+});
 
 xis.addEventListener('click', function() {
     mostraMenu();
-})
+});
+
+// span2.addEventListener('click', function() {
+//     if(largura > 320) return;
+//     if(largura <= 320) menuEscondido();
+// });
 
 function mostraMenu() {
     if(menu.style.display !== 'block') { 
@@ -19,3 +28,12 @@ else {
     menu.style.display = 'none'
 } 
 }
+
+// function menuEscondido() {
+//     if(menu2.style.display !== 'flex') { 
+//         menu2.style.display = 'flex'
+//     }
+//     else {
+//         menu2.style.display = 'none'
+//     } 
+// }
