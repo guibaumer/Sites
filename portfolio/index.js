5a75dda2fbe660e6c5a7c0e637fbe651fbcdb90f
guibaumer <117869143+guibaumer@users.noreply.github.com>
2023-05-09 14:03:42 -0300

    ok

--- portfolio/index.js
+++ portfolio/index.js
@@ -6,14 +6,14 @@ console.log(spanClose, spanMenu, hidden)
 
 spanMenu.addEventListener('click', () => {
     if(hidden.style.display = 'none') {
-        hidden.style.display = 'block'
+        hidden.style.display = 'flex'
     }
 });
 
 spanClose.addEventListener('click', () => {
-    if(hidden.style.display = 'block') {
+    if(hidden.style.display = 'flex') {
         hidden.style.display = 'none'
     } else {
-        hidden.style.display = 'block'
+        hidden.style.display = 'flex'
     }
 });
