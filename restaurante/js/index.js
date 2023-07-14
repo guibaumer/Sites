d040bd7ec37302605d3b90cd553136a1a1e8c96e
guibaumer <117869143+guibaumer@users.noreply.github.com>
2023-07-14 10:25:37 -0300

    novo site

---
+++ restaurante/js/index.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.h1', {
+    rotate: {x:0, y:30, z: 0},
+    duration: 2000,
+});
+sr.reveal('.p-home', {
+    rotate: {x:0, y:0, z: 0},
+    duration: 3000,
+});
+sr.reveal('.menu-h2', {
+    rotate: {x:0, y:50, z: 0},
+    duration: 2000,
+});
