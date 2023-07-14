332841878d703fa6432df2901599aa2392298bf8
guibaumer <117869143+guibaumer@users.noreply.github.com>
2023-07-14 20:30:03 -0300

    acabei

--- restaurante/js/index.js
+++ restaurante/js/index.js
@@ -12,3 +12,26 @@ sr.reveal('.menu-h2', {
     rotate: {x:0, y:50, z: 0},
     duration: 2000,
 });
+
+
+const menu = document.querySelector('.menu');
+const hidden = document.querySelector('.hidden');
+
+menu.addEventListener('click', () => {
+    apareceMenu();
+});
+
+function apareceMenu() {
+    // if(hidden.style.height === '6rem') {
+    //     console.log('sim');
+    // } else {
+    //     console.log('não');
+    // }
+    if(hidden.classList.contains('escondido')) {
+        hidden.classList.remove('escondido');
+        hidden.classList.add('estendido');
+    } else {
+        hidden.classList.add('escondido');
+        hidden.classList.remove('estendido');
+    }
+}
